1c0f0bd4d2d9d07dc56d9be1a5bafa673403481e
Alekhyaa <hialekhyaa@gmail.com>
2024-03-24 18:29:24 -0500

    fe past results table update

--- react-fe/src/App.js
+++ react-fe/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import DataTable from './components/DataTable.js';
 
 function App() {
   const [selectedModel, setSelectedModel] = useState(null);
@@ -38,35 +39,38 @@ function App() {
       <div className="top-right section">
         <h2>Choose a Model</h2>
         <div className="model-buttons">
-          <button onClick={() => handleModelSelect("Model1")}>Model 1</button>
-          <button onClick={() => handleModelSelect("Model2")}>Model 2</button>
-          <button onClick={() => handleModelSelect("Model3")}>Model 3</button>
+          <button onClick={() => handleModelSelect("Model1")}>LCCDE</button>
+          <button onClick={() => handleModelSelect("Model2")}>Tree Based</button>
+          <button onClick={() => handleModelSelect("Model3")}>MTH</button>
         </div>
-      </div>
-      <div className="bottom-right section">
-        <h2>Upload Dataset</h2>
-        <div className="upload-section">
-          <input type="file" onChange={handleDatasetUpload} />
-        </div>
-        <button onClick={handleRunAlgorithm}>Run</button>
       </div>
       <div className="top-left section">
         <h2>Results</h2>
         <div className="results">
-          <p>BENIGN: 0</p>
-          <p>DoS: 0</p>
-          <p>PortScan: 0</p>
-          <p>Bot: 0</p>
-          <p>Infiltration: 0</p>
-          <p>WebAttack: 0</p>
-          <p>BruteForce: 0</p>
+          <div><p>TimeStamp: 0</p></div>
+          <div><p>RunNumber: 0</p></div>
+          <div><p>BENIGN: 0</p></div>
+          <div><p>DoS: 0</p></div>
+          <div><p>PortScan: 0</p></div>
+          <div><p>Bot: 0</p></div>
+          <div><p>Infiltration: 0</p></div>
+          <div><p>WebAttack: 0</p></div>
+          <div><p>BruteForce: 0</p></div>
         </div>
+      </div>
+      <div className="bottom-right section">
+        <h2>Upload Dataset</h2>
+        <div className="data-section">
+          <button onClick={() => handleModelSelect("Data1")}>Dataset 1</button>
+          <button onClick={() => handleModelSelect("Data2")}>Dataset 2</button>
+        </div>
+        <button onClick={handleRunAlgorithm}>Run</button>
       </div>
       <div className="bottom-left section">
         <h2>Past Results</h2>
         {/* Display past results in a table */}
         <div className="past-results-table">
-          {/* Table rows for past results */}
+          <DataTable></DataTable>
         </div>
         <div className="action-buttons">
           <button onClick={handleViewResults}>View</button>
