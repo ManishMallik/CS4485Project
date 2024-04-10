14f6d959b304c7fc7191709314631bdf510814c0
ManishMallik <manish03mallik@gmail.com>
2024-04-10 16:40:06 -0500

    Testing out the tree-based model

--- og-be/tree_based_ids_globecom19.py
+++ og-be/tree_based_ids_globecom19.py
@@ -382,48 +382,48 @@ def main():
 
     """### Stacking model construction"""
 
-    base_predictions_train = pd.DataFrame( {
-        'DecisionTree': dt_train.ravel(),
-            'RandomForest': rf_train.ravel(),
-        'ExtraTrees': et_train.ravel(),
-        'XgBoost': xg_train.ravel(),
-        })
-    base_predictions_train.head(5)
+    # base_predictions_train = pd.DataFrame( {
+    #     'DecisionTree': dt_train.ravel(),
+    #         'RandomForest': rf_train.ravel(),
+    #     'ExtraTrees': et_train.ravel(),
+    #     'XgBoost': xg_train.ravel(),
+    #     })
+    # base_predictions_train.head(5)
 
-    dt_train=dt_train.reshape(-1, 1)
-    et_train=et_train.reshape(-1, 1)
-    rf_train=rf_train.reshape(-1, 1)
-    xg_train=xg_train.reshape(-1, 1)
-    dt_test=dt_test.reshape(-1, 1)
-    et_test=et_test.reshape(-1, 1)
-    rf_test=rf_test.reshape(-1, 1)
-    xg_test=xg_test.reshape(-1, 1)
+    # dt_train=dt_train.reshape(-1, 1)
+    # et_train=et_train.reshape(-1, 1)
+    # rf_train=rf_train.reshape(-1, 1)
+    # xg_train=xg_train.reshape(-1, 1)
+    # dt_test=dt_test.reshape(-1, 1)
+    # et_test=et_test.reshape(-1, 1)
+    # rf_test=rf_test.reshape(-1, 1)
+    # xg_test=xg_test.reshape(-1, 1)
 
-    x_train = np.concatenate(( dt_train, et_train, rf_train, xg_train), axis=1)
-    x_test = np.concatenate(( dt_test, et_test, rf_test, xg_test), axis=1)
+    # x_train = np.concatenate(( dt_train, et_train, rf_train, xg_train), axis=1)
+    # x_test = np.concatenate(( dt_test, et_test, rf_test, xg_test), axis=1)
 
-    x_test
+    # x_test
 
-    stk = xgb.XGBClassifier().fit(x_train, y_train)
-    y_predict=stk.predict(x_test)
-    y_true=y_test
-    stk_score=accuracy_score(y_true,y_predict)
-    print('Accuracy of Stacking: '+ str(stk_score))
-    precision,recall,fscore,none= precision_recall_fscore_support(y_true, y_predict, average='weighted')
-    print('Precision of Stacking: '+(str(precision)))
-    print('Recall of Stacking: '+(str(recall)))
-    print('F1-score of Stacking: '+(str(fscore)))
-    print(classification_report(y_true,y_predict))
-    cm=confusion_matrix(y_true,y_predict)
-    f,ax=plt.subplots(figsize=(5,5))
-    sns.heatmap(cm,annot=True,linewidth=0.5,linecolor="red",fmt=".0f",ax=ax)
-    plt.xlabel("y_pred")
-    plt.ylabel("y_true")
+    # stk = xgb.XGBClassifier().fit(x_train, y_train)
+    # y_predict=stk.predict(x_test)
+    # y_true=y_test
+    # stk_score=accuracy_score(y_true,y_predict)
+    # print('Accuracy of Stacking: '+ str(stk_score))
+    # precision,recall,fscore,none= precision_recall_fscore_support(y_true, y_predict, average='weighted')
+    # print('Precision of Stacking: '+(str(precision)))
+    # print('Recall of Stacking: '+(str(recall)))
+    # print('F1-score of Stacking: '+(str(fscore)))
+    # print(classification_report(y_true,y_predict))
+    # cm=confusion_matrix(y_true,y_predict)
+    # f,ax=plt.subplots(figsize=(5,5))
+    # sns.heatmap(cm,annot=True,linewidth=0.5,linecolor="red",fmt=".0f",ax=ax)
+    # plt.xlabel("y_pred")
+    # plt.ylabel("y_true")
     #plt.show()
 
-    print("Predicted Labels:")
-    for label in y_predict:
-        print(label)
+    # print("Predicted Labels:")
+    # for label in y_predict:
+    #     print(label)
 
     return convert_to_dict(y_predict)
 
@@ -489,3 +489,6 @@ def convert_to_dict(y_predict):
 
     # for label in y_predict_loaded:
     #     print(label)
+
+if __name__ == "__main__":
+    main()
